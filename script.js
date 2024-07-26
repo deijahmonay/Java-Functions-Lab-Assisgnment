382e69764312b5fc704eed00ba6fc428b00cafd2
deijahmonay <deijah@shack15.com>
2024-07-25 21:23:34 -0400

    Completed exercise 4

--- script.js
+++ script.js
@@ -92,7 +92,9 @@ should return 'johnsmith@example.com'.
 Complete the exercise in the space below:
 */
 
-
+function generateEmail(name,domain) {
+    return (`${name}@${domain}`)
+}
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
@@ -115,6 +117,21 @@ console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
 
 
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+
+// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
 
 
