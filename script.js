89fca15ce83c2e6c77dfb714dc4c6022f604f8f6
deijahmonay <deijah@shack15.com>
2024-07-25 21:38:33 -0400

    starting exercise 6

--- script.js
+++ script.js
@@ -124,6 +124,20 @@ without explicitly typing out the console.log -- please let me know if this is o
 explicit "console.log"
 
 
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
 
 
